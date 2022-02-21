76aba5091cef46c5c7bc9524b3f29ec31eb37285
ssulkuma <95386916+ssulkuma@users.noreply.github.com>
2022-02-21 12:43:13 +0200

    added error management

--- read_02.c
+++ read_02.c
@@ -6,11 +6,19 @@
 /*   By: ssulkuma <ssulkuma@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/18 19:00:32 by ssulkuma          #+#    #+#             */
-/*   Updated: 2022/02/18 20:01:06 by ssulkuma         ###   ########.fr       */
+/*   Updated: 2022/02/21 12:33:51 by ssulkuma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
+
+static void	invalid_map_error(char *line, int fd)
+{
+	free(line);
+	close(fd);
+	ft_putendl("error: invalid map");
+	exit(1);
+}
 
 void	check_valid_chars(char *line, int fd)
 {
@@ -20,13 +28,12 @@ void	check_valid_chars(char *line, int fd)
 	while (line[index] != '\0')
 	{
 		if (ft_isdigit(line[index]) || line[index] == ' ' || line[index] == '-')
-			index++;
-		else
 		{
-			free(line);
-			close(fd);
-			ft_putendl("error: invalid map");
-			exit(1);
+			if (line[index] == '-' && line[index++] == '-')
+				invalid_map_error(line, fd);
+			index++;
 		}
+		else
+			invalid_map_error(line, fd);
 	}
 }
