198b3b6627b69c756d18dde310333112cb0b8424
ssulkuma <95386916+ssulkuma@users.noreply.github.com>
2022-02-25 12:45:47 +0200

    additions to draw

--- draw.c
+++ draw.c
@@ -6,7 +6,7 @@
 /*   By: ssulkuma <ssulkuma@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/22 19:05:50 by ssulkuma          #+#    #+#             */
-/*   Updated: 2022/02/24 18:23:11 by ssulkuma         ###   ########.fr       */
+/*   Updated: 2022/02/25 12:44:09 by ssulkuma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,19 +20,42 @@ static void	draw_pixel_to_image(t_mlx *mlx, int x, int y, int color)
 	*(unsigned int *)dest = color;
 }
 
+static float	get_max_delta(float step_x, float step_y)
+{
+	if (step_x < 0)
+		step_x *= -1;
+	if (step_y < 0)
+		step_y *= -1;
+	if (step_x > step_y)
+		return (step_x);
+	else
+		return (step_y);
+}
+
 static void	draw_algorithm(t_mlx *mlx, t_map *map)
 {
 	float	step_x;
 	float	step_y;
+	float	max_delta;
 
 	mlx->start_z = map->map[(int)mlx->start_y][(int)mlx->start_x];
 	mlx->end_z = map->map[(int)mlx->end_y][(int)mlx->end_x];
 	step_x = mlx->end_x - mlx->start_x;
 	step_y = mlx->end_y - mlx->start_x;
+	max_delta = get_max_delta(step_x, step_y);
+	step_x /= max_delta;
+	step_y /= max_delta;
 	mlx->image = mlx_new_image(mlx->connection, 500, 500);
 	mlx->address = mlx_get_data_addr(mlx->image, &mlx->bits_per_pixel,
 			&mlx->line_len, &mlx->endian);
-	draw_pixel_to_image(mlx, 10, 10, 0x00FF0000);
+	while (mlx->start_x - mlx->end_x || mlx->start_y - mlx->end_y)
+	{
+		draw_pixel_to_image(mlx, mlx->start_x, mlx->start_y, 0x00FF0000);
+		mlx->start_x += step_x;
+		mlx->start_y += step_y;
+		if (mlx->start_x < 0 || mlx->start_x > 500 || mlx->start_y < 0 || mlx->start_y > 500)
+			break ;
+	}
 }
 
 void	draw(t_mlx *mlx, t_map *map)
