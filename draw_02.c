e13c051c0a2094e98030e11479553ecaa555f710
ssulkuma <95386916+ssulkuma@users.noreply.github.com>
2022-03-02 12:14:45 +0200

    removed extra tabs

--- draw_02.c
+++ draw_02.c
@@ -6,7 +6,7 @@
 /*   By: ssulkuma <ssulkuma@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/28 19:02:28 by ssulkuma          #+#    #+#             */
-/*   Updated: 2022/03/01 17:10:01 by ssulkuma         ###   ########.fr       */
+/*   Updated: 2022/03/02 12:14:13 by ssulkuma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 void	add_zoom(t_mlx *mlx)
 {
-		mlx->start_x *= mlx->zoom;
-		mlx->start_y *= mlx->zoom;
-		mlx->end_x *= mlx->zoom;
-		mlx->end_y *= mlx->zoom;
+	mlx->start_x *= mlx->zoom;
+	mlx->start_y *= mlx->zoom;
+	mlx->end_x *= mlx->zoom;
+	mlx->end_y *= mlx->zoom;
 }
 
 void	isometric_projection(t_mlx *mlx)
